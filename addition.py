b6c3887338cd67957dd6667821e9cc224e7a2ef4
cactusdungeons <cactusdungeons@protonmail.com>
2025-02-05 17:06:39 -0700

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,8 @@
+# addition.py
+# cactus dungeons | 2025.02.05
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+sum=num1+num2
+
+print("Number of people who believe epstein didn't kill himself:", sum)
